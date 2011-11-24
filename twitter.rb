8f3de030238aac4f2797c0566efd2cb94b1f79f1
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-11-23 20:47:59 -0800

    Currently posts completely public messages

--- twitter.rb
+++ twitter.rb
@@ -1,6 +1,52 @@
 require 'sinatra'
 require 'erb'
 
-get '/' do
-  "ROOT DIRECTORY!!!!"
+$global_id = 1
+
+class Message
+  attr_accessor :contents, :id
+  
+  def initialize(str='', id=0)
+    @contents = str
+    @id = id
+    
+    $global_id += 1
+  end
+end
+
+messages = Hash.new
+
+get /\// do
+  @body = '<form action="post" method="POST">
+  Post Message Here <input type ="text" name="message" />
+  <input type="submit"/>
+  </form>'
+  
+  erb :index
+end
+
+get '/post' do
+  redirect to('/post/all')
+end
+
+post '/post' do
+  @body = '<h3>Congratulations, you have successfully posted something that will be lost</h3>' +
+    params[:message]
+  
+  messages[@global_id] = Message.new(params[:message], @global_id)
+    
+  erb :index
+end
+
+get '/post/:id' do
+  @body = @messages[:id.to_i]
+  
+  erb :index
+end
+
+delete '/post/:id' do
+  @messages[:id.to_i] = nil
+  @body = "Deleted."
+  
+  erb :index
 end
