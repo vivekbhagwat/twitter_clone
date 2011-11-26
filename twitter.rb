77eed56c27e565d5cb2e77fe06dbb04e712d1ce8
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2011-11-26 04:31:09 -0800

    Adding users still

--- twitter.rb
+++ twitter.rb
@@ -1,13 +1,28 @@
 require 'sinatra'
 require 'erb'
 
+enable :sessions
+
 $global_id = 1
+$salt = '0!'
+
+class User
+  attr_accessor :name, :pw, :id, :posts
+  
+  def initialize(str='', password='', id=0)
+    @name = str
+    @pw = password.crypt($salt)
+    @id = id
+    posts = []
+  end
+end
 
 class Message
   attr_accessor :contents, :id, :timestamp, :user
   
-  def initialize(str='', id=0)
+  def initialize(str='', name=nil, id=0)
     @contents = str
+    @user = name
     @id = id
     @timestamp = Time.now.to_s
     
@@ -19,11 +34,15 @@ class Message
   end
   
   def to_s
-    @contents + " posted at " + @timestamp
+    @contents + " by " + @user + ", posted at " + @timestamp
   end
 end
 
 messages = Hash.new
+users = Hash.new
+
+$ORIGINAL_LOGIN = '<a href="/login">Login</a>'
+$footer = $ORIGINAL_LOGIN
 
 get '/' do
   @body = '<form action="post" method="POST">
@@ -34,12 +53,47 @@ get '/' do
   erb :index
 end
 
+post '/' do
+  redirect('/')
+end
+
+post '/logout' do
+  session = Hash.new
+  $footer = $ORIGINAL_LOGIN
+  redirect('/')
+end
+
+get '/login' do
+  @body = '<form action="" method="POST">
+  Username <input type ="text" name="username" />
+  Password <input type ="password" name="password" />
+  <input type="submit"/>
+  </form>'
+  
+  erb :index
+end
+
+post '/login' do
+  # @body = params[:username] + " " + params[:password] + " " + params[:password].to_s.crypt('0!')
+  user = params[:username]
+  
+  if users[user] && users[user].pw == params[:password].crypt($salt)
+    session = Hash.new
+    session[user] = user
+    $footer = '<a href="/logout">Logout, ' + user + '</a>'
+    redirect('/')
+  else
+    status, headers, body = call env.merge("PATH_INFO" => 'login')
+    return [status, headers, body.map(&:upcase)]
+  end
+end
+
 get '/post' do
   redirect to('/post/all')
 end
 
 post '/post' do
-  m = Message.new(params[:message], $global_id.to_i)
+  m = Message.new(params[:message], '', $global_id.to_i)
   messages[$global_id.to_i] = m
     
   @body = '<h3>Congratulations, you have successfully posted </h3>' +
